9fbffeb874c8afe1cc90a20c7f089c2167454bb1
Max Kalika <mkalika@apple.com>
2017-12-15 10:02:40 -0800

    Issue #43: (lintian) Set distribution to Xenial and add maintainer version suffix.

--- release.sh
+++ release.sh
@@ -16,7 +16,11 @@ echo
 	/usr/bin/curl -L -s https://raw.github.com/mk23/sandbox/master/misc/release.py ||
 	echo 'raise Exception("unable to load release.py")'
 ) |
-	exec /usr/bin/env python2.7 - ${COMMIT} --release stable -e lib/__init__.py "__version__ = '{version}'" "$@"
+	exec /usr/bin/env python2.7 - ${COMMIT} \
+		--release=xenial \
+		--append=-upstream1 \
+		--extra lib/__init__.py "__version__ = '{version}'" \
+		"$@"
 
 if [ -n "${COMMIT}" ] ; then
 	echo
